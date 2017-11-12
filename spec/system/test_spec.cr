52563ef2816bce40786d6fbd5fac179b2fe9a15c
Mark Siemers <mark.siemers@gmail.com>
2017-11-11 22:50:53 -0800

    Undo the things that didn't help

--- spec/system/test_spec.cr
+++ spec/system/test_spec.cr
@@ -4,19 +4,17 @@ class SomeFeature < Spec::System::Test
   describe "Amber Framework website" do
     scenario "user visits amber framework and sees getting started button" do
       visit "http://www.amberframework.org"
-      sleep 3
-      timeout 2000
-      click_on(:css, "header a.btn.btn-primary")
-      sleep 3
 
+      timeout 1000
+      click_on(:css, "header a.btn.btn-primary")
+      wait 2000
       element(:tag_name, "body").text.should contain "Introduction"
     end
 
     scenario "user visits amberframwork homepage and sees logo" do
       visit "http://www.amberframework.org"
 
-      sleep 3
-      timeout 2000
+      wait 2000
       element(:class_name, "img-amber-logo").attribute("src").should match(
         %r(https://www.amberframework.org/assets/img/amber-logo-t-bg.png)
       )
