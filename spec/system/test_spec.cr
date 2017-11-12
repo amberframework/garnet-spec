5dfe58f00661913964bb04d041ac7c466e4e66ab
Mark Siemers <mark.siemers@gmail.com>
2017-11-11 22:25:58 -0800

    Add sleep commands in system tests

--- spec/system/test_spec.cr
+++ spec/system/test_spec.cr
@@ -4,10 +4,10 @@ class SomeFeature < Spec::System::Test
   describe "Amber Framework website" do
     scenario "user visits amber framework and sees getting started button" do
       visit "http://www.amberframework.org"
-      wait 2000
+      sleep 3
       timeout 2000
       click_on(:css, "a.nav-link")
-      wait 4000
+      sleep 3
 
       element(:tag_name, "body").text.should contain "Introduction"
     end
@@ -15,7 +15,8 @@ class SomeFeature < Spec::System::Test
     scenario "user visits amberframwork homepage and sees logo" do
       visit "http://www.amberframework.org"
 
-      wait 2000
+      sleep 3
+      timeout 2000
       element(:class_name, "img-amber-logo").attribute("src").should match(
         %r(https://www.amberframework.org/assets/img/amber-logo-t-bg.png)
       )
